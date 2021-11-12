de5357992ad5f3d20172df42156d6a50c03b3e3d
Jane Moroz <Moroz.jane@yahoo.com>
2021-11-12 12:59:45 +0300

    Updated Header Navigation

--- js/script.js
+++ js/script.js
@@ -1,27 +1,98 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("header__slide");
-  var dots = document.getElementsByClassName("header__dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("header__slide");
+//   var dots = document.getElementsByClassName("header__dot");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+// }
+
+///////////////////////////////////////
+// Header Slider
+const slider = function () {
+  const slides = document.querySelectorAll(".header__slide");
+  const dotContainer = document.querySelector(".header__slide-buttons-box");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".header__dot")
+      .forEach((dot) => dot.classList.remove("active"));
+
+    document
+      .querySelector(`.header__dot[data-slide="${slide}"]`)
+      .classList.add("active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) =>
+        (s.style.transform = `translateX(${
+          100 * (i - slide)
+        }%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    activateDot(0);
+  };
+  init();
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("header__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
